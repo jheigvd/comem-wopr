01c740cbfce05af0447b5f3fc24a76b861702cb9
Juliane Badoux <jheigvd@gmail.com>
2023-12-15 16:12:30 +0100

    Modification des textes en français

--- src/app.svelte
+++ src/app.svelte
@@ -22,10 +22,10 @@
   {#if !$currentGame}
     <section class="jumbotron text-center">
       <div class="container">
-        <h1>Start playing now 😎 !</h1>
+        <h1>Commence à jouer maintenant 😎 !</h1>
         <p class='mt-3'>
-          <button type='button' class="btn btn-success btn-lg" on:click={play('random')}>Easy mode</button>
-          <button type='button' class="btn btn-danger btn-lg" on:click={play('wopr')}>Play against the WOPR !</button>
+          <button type='button' class="btn btn-success btn-lg" on:click={play('random')}>Mode facile</button>
+          <button type='button' class="btn btn-danger btn-lg" on:click={play('wopr')}>Joue à nouveau à WOPR !</button>
         </p>
       </div>
     </section>
@@ -38,8 +38,8 @@
   {#if $currentGame && $currentGame.state !== 'playing'}
     <section class="text-center mt-4">
       <p>
-        <button type='button' class="btn btn-success btn" on:click={play('random')}>Play again in easy mode !</button>
-        <button type='button' class="btn btn-danger btn" on:click={play('wopr')}>Play again against the WOPRm !</button>
+        <button type='button' class="btn btn-success btn" on:click={play('random')}>Joue à nouveau en mode facile !</button>
+        <button type='button' class="btn btn-danger btn" on:click={play('wopr')}>Joue à nouveau contre le WOPRm !</button>
       </p>
     </section>
   {/if}
